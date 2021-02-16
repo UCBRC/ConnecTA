16446154f51ee66d71350c4760fb7717aaf7eeaa
Qingyang Hu <me@hqy.moe>
2021-02-16 21:20:11 +0800

    Fix permission

--- prod.sh
+++ prod.sh
@@ -1,12 +1,23 @@
+# Dependencies
 git submodule update --init --recursive --remote
 npm install
 composer install --ignore-platform-reqs
 composer dump-autoload --no-dev --classmap-authoritative
-cd public/assets
+
+# Legacy Console
+cd public/assets || exit
 bower install --allow-root
 cd ../..
+
+# Build Vue
 export NODE_OPTIONS="--max-old-space-size=2048"
 ./node_modules/.bin/encore production
+
+# Symfony Warm Up
 bin/console doctrine:schema:update --force
 bin/console cache:clear
 bin/console cache:warmup --env=prod
+
+# Fix Permissions
+chown -R www-data ./
+chgrp -R www-data ./
